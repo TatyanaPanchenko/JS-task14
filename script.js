bac533f54f9a356ef0ab31b974b9ff59e13a84e0
TatyanaPanchenko <tatyanapanchenko.web@gmail.com>
2024-08-20 12:13:04 +0300

    Change function render

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
 const postsList = document.querySelector(".posts-list");
-
+const renderPosts = (element) => {
+  const newItemTitle = element.title[0].toUpperCase() + element.title.slice(1);
+  const list = document.createElement("div");
+  list.classList.add("card");
+  list.innerHTML = `
+<input type="checkbox">
+  <div class="card-inner"><h2>Post ${element.id}</h2>
+    <div>${newItemTitle}</div></div>
+<button class="delete">Delete</button>
+`;
+  postsList.appendChild(list);
+};
 const posts = fetch("https://jsonplaceholder.typicode.com/posts")
   .then((response) => response.json())
   .then((json) => {
     json.forEach((item) => {
-      const newItemTitle = item.title[0].toUpperCase() + item.title.slice(1);
-      const list = document.createElement("div");
-      list.classList.add("card");
-      list.innerHTML = `
-    <input type="checkbox">
-			<div class="card-inner"><h2>Post ${item.id}</h2>
-        <div>${newItemTitle}</div></div>
-<button class="delete">Delete</button>
-    `;
-      postsList.appendChild(list);
+      renderPosts(item);
     });
   });
 
@@ -22,7 +24,6 @@ postsList.addEventListener("click", (event) => {
     event.target.parentElement.classList.toggle("checked");
   }
   const checkedElements = document.querySelectorAll(".checked");
-  console.log(checkedElements);
   if (event.target.matches(".delete")) {
     event.target.parentElement.remove();
     checkedElements.forEach((item) => item.remove());
